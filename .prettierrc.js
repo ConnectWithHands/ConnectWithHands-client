015a5cd796ecd6ad43894d68f6c05d8a6698a038
hansol <stellanew21@gmail.com>
2022-07-02 23:21:07 +0900

    Chore: prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,11 @@
 module.exports = {
   semi: true,
+  singleQuote: false,
   tabWidth: 2,
 	useTabs: false,
   bracketSpacing: true,
   trailingComma: "all",
   printWidth: 80,
   arrowParens: "always",
+  endOfLine: "auto",
 };
