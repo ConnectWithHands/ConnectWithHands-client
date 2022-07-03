af482e1152999ee69476ba24b18545e3904a6c82
hansol <stellanew21@gmail.com>
2022-07-04 01:03:11 +0900

    Style: add flex in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 import GlobalStyle from "./components/common/globalStyle";
 import Main from "./pages/Main";
@@ -8,15 +9,21 @@ import Communication from "./pages/Communication";
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/practice" element={<Practice />} />
         <Route path="/communication" element={<Communication />} />
       </Routes>
-    </>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+`;
