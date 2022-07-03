ffff7b9de1da2ac1407ff3c6fd15e16879c74c30
hansol <stellanew21@gmail.com>
2022-07-03 22:24:12 +0900

    Feat: add pages folder for atomic design atomic design을 위한 pages 폴더 생성 및 컴포넌트 생성 - Main, Practice 및 Communication

---
+++ src/pages/Practice.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Practice() {
+  return <Container></Container>;
+}
+
+export default Practice;
+
+const Container = styled.div`
+  display: flex;
+  width: 360px;
+  height: 640px;
+`;
