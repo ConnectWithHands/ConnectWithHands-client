e6282d1ec7275db553c915713ede241a4198ae1e
hansol <stellanew21@gmail.com>
2022-07-04 00:59:03 +0900

    Add: add images for example 한글 자음, 모음 및 알파벳에 대한 대표 이미지 추가

---
+++ src/assets/language/index.js
@@ -0,0 +1,11 @@
+import consonant from "./hangul_consonant.png";
+import vowel from "./hangul_vowel.png";
+import alphabet from "./alphabet.png";
+
+const language = {
+  consonant,
+  vowel,
+  alphabet,
+};
+
+export default language;
