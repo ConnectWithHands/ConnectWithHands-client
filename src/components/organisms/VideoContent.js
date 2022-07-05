e5ffa27d88cc150b69a504a581a196f767bb7d6b
hansol <stellanew21@gmail.com>
2022-07-05 16:14:32 +0900

    Refactor: move Button to upper component and change CSS style

--- src/components/organisms/VideoContent.js
+++ src/components/organisms/VideoContent.js
@@ -6,16 +6,11 @@ import Video from "../atoms/Video";
 import Canvas from "../atoms/Canvas";
 import Button from "../atoms/Button";
 
-function VideoContent({ onClick, facingMode, webcamRef, canvasRef }) {
+function VideoContent({ facingMode, webcamRef, canvasRef }) {
   return (
     <Container>
-      <Button className="small" onClick={onClick}>
-        카메라 전환
-      </Button>
-      <Wrapper>
-        <Video facingMode={facingMode} ref={webcamRef} />
-        <Canvas ref={canvasRef} />
-      </Wrapper>
+      <Video facingMode={facingMode} ref={webcamRef} />
+      <Canvas ref={canvasRef} />
     </Container>
   );
 }
@@ -25,12 +20,12 @@ export default VideoContent;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
-`;
-
-const Wrapper = styled.div`
+  background-color: black;
+  width: 100vw;
   position: relative;
 `;
+
+const Wrapper = styled.div``;
 
 VideoContent.propTypes = {
   onClick: PropTypes.func,
