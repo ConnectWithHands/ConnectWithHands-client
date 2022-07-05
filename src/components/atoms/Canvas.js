847fe2b7f696404f882cbe521f222785d35650d0
hansol <stellanew21@gmail.com>
2022-07-05 15:09:08 +0900

    Feat: add atom file for canvas

---
+++ src/components/atoms/Canvas.js
@@ -0,0 +1,28 @@
+import React, { forwardRef } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+function Canvas(props, ref) {
+  return <StyledCanvas ref={ref} />;
+}
+
+export default forwardRef(Canvas);
+
+const StyledCanvas = styled.canvas`
+  position: absolute;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  text-align: center;
+  z-index: 4;
+  width: auto;
+  height: auto;
+`;
+
+Canvas.propTypes = {
+  props: PropTypes.string,
+  ref: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.elementType }),
+  ]),
+};
