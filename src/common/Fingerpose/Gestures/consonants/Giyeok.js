046dd3d349cd898cbd6217328f1fbbd7e3aaadb3
handol <stellanew21@gmail.com>
2022-07-10 01:21:22 +0900

    Refactor: test gestures

--- src/common/Fingerpose/Gestures/consonants/Giyeok.js
+++ src/common/Fingerpose/Gestures/consonants/Giyeok.js
@@ -3,6 +3,8 @@ import {
   Finger,
   FingerCurl,
   FingerDirection,
+  FingerPosition,
+  FingerAxis,
 } from "../../FingerDescription";
 import GestureDescription from "../../GestureDescription";
 
@@ -12,23 +14,29 @@ giyeok.addCurl(Handedness.Left, Finger.Thumb, FingerCurl.NoCurl, 1.0);
 giyeok.addDirection(
   Handedness.Left,
   Finger.Index,
-  FingerDirection.HorizontalLeft,
+  FingerDirection[FingerAxis.XY].HorizontalLeft,
   0.8,
 );
 // giyeok.addDirection(Finger.Index, FingerDirection.HorizontalRight, 0.8);
 giyeok.addDirection(
   Handedness.Left,
   Finger.Index,
-  FingerDirection.DiagonalDownLeft,
+  FingerDirection[FingerAxis.XY].DiagonalDownLeft,
   0.8,
 );
 
-giyeok.addCurl(Handedness.Left, Finger.Index, FingerCurl.NoCurl, 1.0);
+giyeok.addCurl(
+  Handedness.Left,
+  Finger.Index,
+  FingerCurl.NoCurl,
+  1.0,
+  FingerPosition.Back,
+);
 giyeok.addCurl(Handedness.Left, Finger.Index, FingerCurl.HalfCurl, 0.7);
 giyeok.addDirection(
   Handedness.Left,
   Finger.Index,
-  FingerDirection.VerticalDown,
+  FingerDirection[FingerAxis.XY].VerticalDown,
   0.8,
 );
 
