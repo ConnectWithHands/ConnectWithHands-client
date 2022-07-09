b375e0dc84730ee1134a34a954ad7d0e2ffb35e2
handol <stellanew21@gmail.com>
2022-07-10 01:17:22 +0900

    Feat: add constants for letters and PRACTICE_DETECTED

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+import { COMMUNICATION_LIST } from "./communication";
+import { Letter, lengthOfLetter } from "./letter";
+import { PRACTICE_LIST, PRACTICE_TITLE, PRACTICE_DETECTED } from "./practice";
+import { FACING_MODE } from "./webcam";
+
+export {
+  COMMUNICATION_LIST,
+  Letter,
+  lengthOfLetter,
+  PRACTICE_LIST,
+  PRACTICE_TITLE,
+  PRACTICE_DETECTED,
+  FACING_MODE,
+};
