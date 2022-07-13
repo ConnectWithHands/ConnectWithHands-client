ea7611ce6d0874dc327ee7c83e2f07a99a2106d2
hansol <stellanew21@gmail.com>
2022-07-14 00:22:20 +0900

    Remove: alphabet gestures

--- src/common/Fingerpose/Gestures/index.js
+++ src/common/Fingerpose/Gestures/index.js
@@ -1,13 +1,11 @@
 import words from "./words";
 import consonants from "./consonants";
 import vowels from "./vowels";
-import alphabet from "./alphabet";
 
 const Gestures = {
   words,
   consonants,
   vowels,
-  alphabet,
 };
 
 export default Gestures;
